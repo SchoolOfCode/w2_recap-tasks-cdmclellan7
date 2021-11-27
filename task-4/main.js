aa3831886b420fa0b142385374ccd16af3c5fd55
casey <cassandra.d.mclellan@gmail.com>
2021-11-27 12:01:15 +0000

    task 4: priority input added

--- task-4/main.js
+++ task-4/main.js
@@ -1,18 +1,22 @@
-const list = document.querySelector("#list");
-
 const importantThings = [];
 let currentInput = "";
+let currentPriority = 0;
 
+const list = document.querySelector("#list");
 const inputField = document.querySelector("#important-thing");
 const addButton = document.querySelector("#add-to-list");
+const priorityInput = document.querySelector("#priority-input");
 
 inputField.addEventListener("keyup", (e) => currentInput = e.target.value);
+priorityInput.addEventListener("input", (e) => currentPriority = e.target.value);
 addButton.addEventListener("click", addItemToList);
 
 function addItemToList() {
-    importantThings.push(currentInput);
+    const currentItem = {text: currentInput, priority: currentPriority};
+    importantThings.push(currentItem);
     const newLi = document.createElement('li');
-    newLi.innerText = currentInput;
+    newLi.innerText = `${currentInput}, Priority: ${currentPriority}`;
     list.appendChild(newLi);
 }
+
 
