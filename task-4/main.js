756691ce6952bc628b0a35aa460efc6d4472273f
casey <cassandra.d.mclellan@gmail.com>
2021-11-27 11:33:30 +0000

    task 4: can add input to the array

--- task-4/main.js
+++ task-4/main.js
@@ -1,1 +1,12 @@
+const importantThings = [];
+let currentInput = "";
+
+const inputField = document.querySelector("#important-thing");
+const addButton = document.querySelector("#add-to-list");
+
+inputField.addEventListener("keyup", (e) => currentInput = e.target.value);
+addButton.addEventListener("click", () => {
+    importantThings.push(currentInput);
+    console.log(importantThings);
+});
 
