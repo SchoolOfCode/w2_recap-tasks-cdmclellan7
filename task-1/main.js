0524ef744b90a141ed290e6e11e040793ddb772c
casey <cassandra.d.mclellan@gmail.com>
2021-11-27 10:51:08 +0000

    completed task 1

--- task-1/main.js
+++ task-1/main.js
@@ -10,3 +10,16 @@ const celebs = [
   "Richard Osman",
   "Emma Thompson"
 ];
+
+function makeLegend(name) {
+  return `${name} is now a legend.`;
+}
+
+let legendaryCelebs = celebs.map( (name) => makeLegend(name));
+
+const vowels = ["A", "E", "I", "O", "U"]
+
+let vowelCelebs = celebs.filter( (name) => vowels.includes(name[0]));
+
+console.log("legendaryCelebs", legendaryCelebs);
+console.log("vowelCelebs", vowelCelebs);
