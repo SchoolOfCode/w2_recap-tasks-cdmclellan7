94a27bb2fb481dc9ec498df71bb89b335633b3e1
casey <cassandra.d.mclellan@gmail.com>
2021-11-27 11:05:20 +0000

    task 2: counter increments

--- task-2/main.js
+++ task-2/main.js
@@ -1,1 +1,9 @@
+let counterP = document.querySelector("#counter");
+let count = 0;
 
+function incrementCounter() {
+    count++;
+    counterP.innerText = count;
+}
+
+setInterval(incrementCounter, 1000);
